6e3855c2229a0e4693f9cdf0fb42b115c930a5ae
Devin Alexander <devinmalexander@gmail.com>
2024-12-28 09:31:43 -0600

    Level Selection

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 const ANIMAL = preload("res://scenes/animal/animal.tscn")
+const MAIN = preload("res://scenes/main/main.tscn")
 
 @onready var animal_start: Marker2D = $AnimalStart
 
@@ -13,7 +14,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().change_scene_to_packed(MAIN)
 
 func add_animal() -> void:
 	var animal = ANIMAL.instantiate()
