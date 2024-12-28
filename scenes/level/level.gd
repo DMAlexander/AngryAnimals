a0a163020cfa0e096afa27bb1fbae368772491fc
Devin Alexander <devinmalexander@gmail.com>
2024-12-28 11:46:27 -0600

    Final touches - UI, etc

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 
 const ANIMAL = preload("res://scenes/animal/animal.tscn")
-const MAIN = preload("res://scenes/main/main.tscn")
 
 @onready var animal_start: Marker2D = $AnimalStart
 
@@ -11,11 +10,6 @@ func _ready() -> void:
 	add_animal()
 	SignalManager.on_animal_died.connect(add_animal)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_ESCAPE):
-		get_tree().change_scene_to_packed(MAIN)
 
 func add_animal() -> void:
 	var animal = ANIMAL.instantiate()
